7e91b573db9e89e3d4fde14278d7e76dcc84b8dc
geborskimateusz <mateuszgeborski@gmail.com>
2021-03-18 22:00:19 +0100

    joi validation

--- src/middlewares/schema-validator.js
+++ src/middlewares/schema-validator.js
@@ -1,9 +1,9 @@
 const { ValidationError } = require('../errors/validation-error')
 const { genre } = require('../models/genre')
+const { createMovie } = require('../schemas/movie')
 
 const validateGenres = (genres) => {
     const fromDB = genre.find()
-
     const validationArr = genres.reduce((acc, e) => {
         if (!fromDB.includes(e)) {
             acc.push(e)
@@ -12,12 +12,21 @@ const validateGenres = (genres) => {
     }, [])
 
     if (validationArr.length !== 0) {
-        console.log("err")
         throw new ValidationError(`Missing genres ${validationArr}`);
     }
 
     return genres;
-
 }
 
-module.exports = { validateGenres }
+const validateMovieSchema = () => {
+
+    return (req, res, next) => {
+        const result = createMovie.validate(req.body);
+        if (result.error) {
+            return res.status(400).send(result)
+        }
+        next()
+    };
+}
+
+module.exports = { validateGenres, validateMovieSchema }
---
+++ src/schemas/movie.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+const { validateGenres } = require('../middlewares/schema-validator')
+const createMovie = Joi.object({
+    genres: Joi.array().items(Joi.string())
+        .custom(genres => {
+            console.log("here1")
+            validateGenres(genres);
+            return genres
+        })
+        .error(errors => {
+            errors.forEach(err => {
+                switch (err.code) {
+                    case "any.custom":
+                        err.message = "Invalid genre"
+                        break
+                    default:
+                        break;
+
+                }
+            })
+            return errors;
+        }),
+    // title: Joi.string().max(255).required(),
+    // year: Joi.number().required(),
+    // runtime: Joi.number().required(),
+    // director: Joi.string().max(255).required(),
+    // actors: Joi.string(),
+    // plot: Joi.string(),
+    // posterUrl: Joi.string()
+
+}).strict();
+
+module.exports = { createMovie };
